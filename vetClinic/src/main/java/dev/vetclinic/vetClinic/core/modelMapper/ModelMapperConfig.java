e42b6a8b6a4c2ed81bc80dcc6b68885cce5e8714
Nese <neseilhannn@gmail.com>
2024-07-21 02:39:10 +0300

    model mapper.

---
+++ vetClinic/src/main/java/dev/vetclinic/vetClinic/core/modelMapper/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package dev.vetclinic.vetClinic.core.modelMapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper getModelMapper(){
+        return new ModelMapper();
+    }
+}
